5f4c19229ad912f5f3310622347a874f28169670
Douglas Leal <douglas.leal@live.com>
2023-03-11 11:56:51 -0300

    Nota - validação de dados da requisição criada

--- src/routes/notasRoutes.js
+++ src/routes/notasRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 
 import Controller from "../controllers/NotaController.js";
+import validation from "../validations/notaValidation.js";
 
 const router = express.Router();
 
 router
     .get("/", Controller.listar)
     .get("/:id", Controller.buscarPorId)
-    .post("/", Controller.criar)
-    .put("/:id", Controller.atualizar)
+    .post("/", validation, Controller.criar)
+    .put("/:id", validation, Controller.atualizar)
     .delete("/:id", Controller.excluir);
 
 export default router;
